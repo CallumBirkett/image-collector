0cf5f3bf9d10e78b2add985496baca2ae13c300c
Callum Birkett <callum@callums-air.mynet>
2020-04-25 11:54:45 +0100

    Removed unnecessary function. Implemented file list in main().

--- imagefind.py
+++ imagefind.py
@@ -2,27 +2,9 @@
 # imagefind.py - Finds image files in a user-inputted directory and copies them to a
 #  new folder in that directory.
 
-# TODO: Create tests.
-
 import os
 import shutil
 import sys
-
-
-def getListOfFiles(dirName):
-    # Get list of objects in diretory.
-    listDir = os.listdir(dirName)
-    allFiles = list()
-    # For each object, get type. If object = dir, repeat function.
-    for entry in listDir:
-        # Get the path of each object.
-        path = os.path.join(dirName, entry)
-        if os.path.isdir(path):
-            allFiles = allFiles + getListOfFiles(path)
-        else:
-            allFiles.append(path)
-
-    return allFiles
 
 
 def main():
@@ -31,11 +13,13 @@ def main():
         os.mkdir(os.path.join(dirName, newFolder))
     # Set path of new dir.
     newPath = os.path.join(dirName, newFolder)
-    # Get a list of filenames in the given dir.
-    listOfFileNames = getListOfFiles(dirName)
+    # Get list of files in provided dir, by walking through file tree.
+    fileList = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        fileList += [os.path.join(dirpath, filename) for filename in filenames]
     # For each filename check for required extension. Generate source path of file
     # and copy to new dir.
-    for filename in listOfFileNames:
+    for filename in fileList:
         if filename.lower().endswith(('.jpeg', '.pdf', '.png', '.gif')):
             sourcepath = os.path.abspath(filename)
             shutil.copy(sourcepath, newPath)
