f6f82d9aa1c3d7d75bb1fa6961f60d61c66fef55
Callum Birkett <callum@callums-air.mynet>
2020-04-25 18:47:03 +0100

    Changed variable names to satisfy pylint. Removed unnecessary module.

--- imagefind.py
+++ imagefind.py
@@ -1,32 +1,39 @@
-#! python3
+'''
+! python3
 # imagefind.py - Finds image files in a user-inputted directory and copies them to a
 #  new folder in that directory.
+'''
 
 import os
 import shutil
-import sys
 
 
 def main():
+    '''
+    Function creates directory to store new files in. Searches file tree for all files.
+    Finds files with required extensions and copies them to new directory.
+    '''
     # Create new directory for copied files.
-    if not os.path.exists(os.path.join(dirName, newFolder)):
-        os.mkdir(os.path.join(dirName, newFolder))
+
+    dir_name = input("Enter your directory: ")
+    new_folder = input("Enter a name for your new folder: ")
+
+    if not os.path.exists(os.path.join(dir_name, new_folder)):
+        os.mkdir(os.path.join(dir_name, new_folder))
     # Set path of new dir.
-    newPath = os.path.join(dirName, newFolder)
+    new_path = os.path.join(dir_name, new_folder)
     # Get list of files in provided dir, by walking through file tree.
-    fileList = list()
-    for (dirpath, dirnames, filenames) in os.walk(dirName):
-        fileList += [os.path.join(dirpath, filename) for filename in filenames]
+    file_list = list()
+    for (dirpath, _, filenames) in os.walk(dir_name):
+        file_list += [os.path.join(dirpath, filename)
+                      for filename in filenames]
     # For each filename check for required extension. Generate source path of file
     # and copy to new dir.
-    for filename in fileList:
+    for filename in file_list:
         if filename.lower().endswith(('.jpeg', '.pdf', '.png', '.gif')):
             sourcepath = os.path.abspath(filename)
-            shutil.copy(sourcepath, newPath)
+            shutil.copy(sourcepath, new_path)
 
-
-dirName = input("Enter your directory: ")
-newFolder = input("Enter a name for your new folder: ")
 
 if __name__ == '__main__':
     main()
